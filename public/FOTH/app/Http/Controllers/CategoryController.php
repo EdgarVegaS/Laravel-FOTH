1240c15dd2b01d1b778ba010ef7463d99b64cdd3
Carlos Brown <cbrownc97@gmail.com>
2020-05-17 22:10:30 -0400

    add validation and request

--- public/FOTH/app/Http/Controllers/CategoryController.php
+++ public/FOTH/app/Http/Controllers/CategoryController.php
@@ -4,29 +4,46 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Category;
+use Illuminate\Support\Facades\Log;
+use App\Http\Requests\CategoryCreateRequest;
 
 class CategoryController extends Controller
 {
-  public function index() {
-    return Category::all();
-  }
+	public function index() {
+		return Category::all();
+	}
 
-  public function show(Category $categoryId) {
-    return $categoryId;
-  }
+	public function show(Category $categoryId) {
+		try {
+			return $categoryId;
+		} catch (Exception $e) {
+			Log::error($e->getMessage());
+			echo 'Error get category id', $e->getMessage(), '\n';
+		}
+	}
 
-  public function create(Request $req) {
-    $category = Category::create($req->all());
-    return response()->json($category, 201);
-  }
+	public function create(CategoryCreateRequest $req) {
+		try {
+			$this->validate($req, [
+				'name' => 'required',
+				'description' => 'required'
+			]);
 
-  public function update(Request $req, Category $categoryId) {
-    $categoryId->update($req->all());
-    return response()->json($categoryId, 200);
-  }
+			$category = Category::create($req->all());
+			return response()->json($category, 201);
+		} catch (Exception $e) {
+			Log::error($e->getMessage());
+			echo 'Error create category', $e->getMessage(), '\n';
+		}
+	}
 
-  public function destroy(Category $categoryId) {
-    $categoryId->delete();
-    return response()->json(null, 204);
-  }
+	public function update(Request $req, Category $categoryId) {
+		$categoryId->update($req->all());
+		return response()->json($categoryId, 200);
+	}
+
+	public function destroy(Category $categoryId) {
+		$categoryId->delete();
+		return response()->json(null, 204);
+	}
 }
