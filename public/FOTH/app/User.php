30c7f15624033b7bb806f9a6385913858713d404
edgar.vega <edgar.vega@metacontrol.cl>
2020-05-19 20:25:21 -0400

    token de registro y loggin

--- public/FOTH/app/User.php
+++ public/FOTH/app/User.php
@@ -5,8 +5,9 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -43,6 +44,16 @@ class User extends Authenticatable
 
     public function favorite() {
       return $this->hasMany('App\Favorite');
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
 
--- public/FOTH/routes/api.php
+++ public/FOTH/routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+Route::post('login', 'APIController@login');
+Route::post('register', 'APIController@register');
+
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::get('logout', 'APIController@logout');
+
 });
